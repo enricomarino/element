b08a0f1be7208ba2190ffc349d9c2456afe93258
Enrico Marino <enrico.marino@email.com>
2013-01-24 12:22:53 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+
+/**
+ * element
+ * Element component
+ * 
+ * @author Enrico Marino
+ * @copyright 2013 Enrico Marino
+ */
+
+/*
+ * Expose `Element`
+ */
+
+module.expose = Element;
+
+/*
+ * Element
+ */
+
+function Element(el) {
+  this.el = el;
+}
